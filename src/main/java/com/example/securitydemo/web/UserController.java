df7c9758c2d84eda1b0f24e5ff5f5081e93f1e33
Leonardo <leohpr@gmail.com>
2018-09-07 21:50:40 -0300

    Add pages

---
+++ src/main/java/com/example/securitydemo/web/UserController.java
@@ -0,0 +1,15 @@
+package com.example.securitydemo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+	@GetMapping("/logged")
+	public String logged() {
+		return "users/logged";
+	}
+}
